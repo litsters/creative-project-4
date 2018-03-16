4cb18234060f3ca325a7b1f2ceca53ffaefe53bf
litsters <snlitster@gmail.com>
2018-03-15 21:58:02 -0600

    changed port since 1342 might not be working for some reason

--- server.js
+++ server.js
@@ -50,4 +50,4 @@ app.delete('/api/searches/:id', (req,res) => {
   res.sendStatus(200);
 });
 
-app.listen(1342, () => console.log('Server listening on port 1342!'));
+app.listen(3001, () => console.log('Server listening on port 3001!'));
